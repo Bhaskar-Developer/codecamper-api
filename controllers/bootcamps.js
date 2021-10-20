d6693d2db53029a9386360ccafe6db20f004308a
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-10-20 20:25:40 +0530

    Get all bootcamps and get single bootcamp route logic implemented.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,45 @@ const Bootcamp = require('../models/Bootcamp')
 //@desc     Get All Bootcamps
 //@route    GET /api/v2/bootcamps
 //@access   Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show All Bootcamps' })
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find()
+    res.status(200).json({
+      success: true,
+      data: bootcamps
+    })
+  } catch(err) {
+    res.status(400).json({
+      success: false
+    })
+  }
 }
 
 //@desc     Get Bootcamp
 //@route    GET /api/v2/bootcamps/:id
 //@access   Private
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Show Bootcamp ${req.params.id}` })
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id)
+    
+    //If the bootcamp does not exist then send 404 response
+    if(!bootcamp) {
+      return res.status(404).json({
+        success: false
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp
+    })
+
+  } catch(err) {
+    //This will be sent if the bootcamp id does not have a proper format i.e. type is not Object Id
+    res.status(500).json({
+      success: false
+    })
+  }
 }
 
 //@desc     Create Bootcamp
