367c95be6fb05bb6ac4e7efe3229060df48af71b
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-11-06 20:22:54 +0530

    Logic added to calculate average cost for a bootcamp when a course is added or removed.

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const colors = require('colors')
 
 const CourseSchema = new mongoose.Schema({
   title: {
@@ -37,5 +38,46 @@ const CourseSchema = new mongoose.Schema({
     required: true
   }
 })
+
+// Static method to calculate average cost of bootcamp
+CourseSchema.statics.getAverageCost = async function(bootcampId){
+  const obj = await this.aggregate([
+    //Run Pipeline
+    //Match the bootcamp id with the courses bootcamp id then calculate the average cost
+    //We want to update average cost field of the bootcamp 
+    //We gather all courses that belong to this bootcamp and calcualte the average cost based on the tution field of the courses
+    //We then update the new average cost to the bootcamps average cost field
+    {
+      $match: {bootcamp: bootcampId}
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: {$avg: '$tuition'}
+      }
+    }
+  ])
+
+  //Update the bootcamp with the new average cost
+  try {
+      await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+      })
+  } catch (err) {
+      console.error(err)
+  }
+}  
+
+//Call static method to update average cost of bootcamp when a new course is created
+CourseSchema.post('save', function() {
+  const course = this //current instance of course
+  course.constructor.getAverageCost(course.bootcamp)
+})
+
+//call static method to update average cost of bootcamp when a course is deleted
+CourseSchema.post('remove', function() {
+  const course = this //current instance of course
+  course.constructor.getAverageCost(course.bootcamp)
+}) 
 
 module.exports = mongoose.model('Course', CourseSchema)
