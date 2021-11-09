fccf7a7c330e25620638ac0e396ea81156803f3b
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-11-09 19:48:21 +0530

    Only user who owns the course can update or delete it. Only user who owns a bootcamp or is an admin can create a new course. Logic added for the same.

--- controllers/courses.js
+++ controllers/courses.js
@@ -54,16 +54,25 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 
 //@desc     Add New Course
 //@route    POST /api/v2/bootcamps/:bootcampId/courses
-//@access   Public
+//@access   Private
 exports.createCourse = asyncHandler(async (req, res, next) => {
     //get the bootcamp id from the req params and set it as a field in the course that is being created
-    req.body.bootcamp = req.params.bootcampId 
+    req.body.bootcamp = req.params.bootcampId
+    //get the user id from the req and set it as a field in the course that is being created
+    req.body.user = req.user.id 
 
     //check if the bootcamp exists
     const bootcamp = await Bootcamp.findById(req.params.bootcampId)
 
     if (!bootcamp) {
         return next(new errorResponse(`Bootcamp not found with the id of ${req.params.bootcampId}`, 404))
+    }
+
+    //If the user is not the owner of the bootcamp then the user cannot add a course
+    //Admin can add courses to any bootcamp
+    //Publisher can add courses to their own bootcamp only
+    if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new errorResponse(`User with id ${req.user.id} is not authorized to add course to the bootcamp with id ${bootcamp._id}`, 401))
     }
 
     //create the course if the bootcamp exists
@@ -78,13 +87,21 @@ exports.createCourse = asyncHandler(async (req, res, next) => {
 
 //@desc     Update Course
 //@route    PUT /api/v2/courses/:id
-//@access   Public
+//@access   Private
 exports.updateCourse = asyncHandler(async (req, res, next) => {
     //check if the course to be updated exists in the database
     let course = await Course.findById(req.params.id)
 
+    //if the course does not exist then return a 404 error
     if (!course) {
         return next(new errorResponse(`Course not found with the id of ${req.params.id}`, 404))
+    }
+
+    //If the user is not the owner of the course then the user cannot update this course
+    //Admin can update any course
+    //Publisher can update courses that are owned by them only
+    if(course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new errorResponse(`User with id ${req.user.id} is not authorized to update course with id ${course._id}`, 401))
     }
 
     //Update the course
@@ -101,13 +118,21 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 
 //@desc     Delete Course
 //@route    PUT /api/v2/courses/:id
-//@access   Public
+//@access   Private
 exports.deleteCourse = asyncHandler(async (req, res, next) => {
     //check if the course to be deleted exists in the database
     const course = await Course.findById(req.params.id)
 
+    //if the course does not exist then return a 404 error
     if (!course) {
         return next(new errorResponse(`Course not found with the id of ${req.params.id}`, 404))
+    }
+
+    //If the user is not the owner of the course then the user cannot delete this course
+    //Admin can delete any course
+    //Publisher can delete courses that are owned by them only
+    if(course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new errorResponse(`User with id ${req.user.id} is not authorized to delete course with id ${course._id}`, 401))
     }
 
     //Delete the course
